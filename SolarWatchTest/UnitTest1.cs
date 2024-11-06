66a0221b45cabe93f6a12ad43ad0f13bee5bfb7e
GogosDani <gogosdaniel04@gmail.com>
2024-11-06 10:37:02 +0100

    Add test project with dependencies

---
+++ SolarWatchTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace SolarWatchTest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}

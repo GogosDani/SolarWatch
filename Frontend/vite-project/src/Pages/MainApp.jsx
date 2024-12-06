b2c11351c5feb7a1f209038b074a415a21bc70c7
GogosDani <gogosdaniel04@gmail.com>
2024-12-06 12:05:13 +0100

    User visit the solar page without login = error message and navigate to the main page

--- Frontend/vite-project/src/Pages/MainApp.jsx
+++ Frontend/vite-project/src/Pages/MainApp.jsx
@@ -1,16 +1,25 @@
 import { useState, useEffect } from "react"
 import api from "../Axios/api"
+import { useNavigate } from "react-router-dom";
+
 
 export default function MainApp() {
 
+    const navigate = useNavigate();
+    const [errorMessage, setErrorMessage] = useState(null);
     const [city, setCity] = useState("");
     const [date, setDate] = useState("");
     const [info, setInfo] = useState({});
 
     useEffect(() => {
-        console.log("------------------asd-------------------")
-        console.log(info)
-    }, [info])
+        const token = localStorage.getItem("token");
+        if (token == null) {
+            setErrorMessage("You must login before access this page!");
+            setTimeout(() => {
+                navigate('/');
+            }, 5000);
+        }
+    }, [navigate])
 
     async function handleSubmit(e) {
         e.preventDefault();
@@ -26,38 +35,48 @@ export default function MainApp() {
     }
 
     return (
-        <div className="main-app-div">
-            <div className="search-form-div">
-                <p className="title-solar"> SolarWatch </p>
-                <form className="search-form" onSubmit={(e) => handleSubmit(e)}>
-                    <label className="search-form-label"> Type a city here </label>
-                    <input className="search-form-input" required onChange={(e) => setCity(e.target.value)} />
-                    <label className="search-form-label"> Add the date </label>
-                    <input className="search-form-input" required onChange={(e) => setDate(e.target.value)} type="date" />
-                    <button className="search-submit" disabled={city === "" ? true : false}> Submit </button>
-                </form>
-            </div>
-            <div className="solar-info-div">
-                {Object.keys(info).length == 0 ? "" : (
-                    <>
-                        <div className="solar-info-top">
-                            <p className="city-info"> {info.city.name} - {info.date}</p>
-                        </div>
-                        <div className="solar-info-bottom">
-                            <div className="left">
-                                <p className="solar-data"> {info.sunrise} </p>
-                                <p className="solar-data"> SUNRISE </p>
-                            </div>
-                            <div className="right">
-                                <p className="solar-data"> {info.sunset} </p>
-                                <p className="solar-data"> SUNSET </p>
-                            </div>
-                        </div>
-                    </>
-                )
-                }
-            </div>
-        </div>
+        <>
+            {errorMessage ? (<h1 className="error-message"> {errorMessage} </h1>) : (
+
+                <div className="main-app-div">
+                    <div className="search-form-div">
+                        <p className="title-solar"> SolarWatch </p>
+                        <form className="search-form" onSubmit={(e) => handleSubmit(e)}>
+                            <label className="search-form-label"> Type a city here </label>
+                            <input className="search-form-input" required onChange={(e) => setCity(e.target.value)} />
+                            <label className="search-form-label"> Add the date </label>
+                            <input className="search-form-input" required onChange={(e) => setDate(e.target.value)} type="date" />
+                            <button className="search-submit" disabled={city === "" ? true : false}> Submit </button>
+                        </form>
+                    </div>
+                    <div className="solar-info-div">
+                        {Object.keys(info).length == 0 ? "" : (
+                            <>
+                                <div className="solar-info-top">
+                                    <p className="city-info"> {info.city.name} - {info.date}</p>
+                                </div>
+                                <div className="solar-info-bottom">
+                                    <div className="left">
+                                        <p className="solar-data"> {info.sunrise} </p>
+                                        <p className="solar-data"> SUNRISE </p>
+                                    </div>
+                                    <div className="right">
+                                        <p className="solar-data"> {info.sunset} </p>
+                                        <p className="solar-data"> SUNSET </p>
+                                    </div>
+                                </div>
+                            </>
+                        )
+                        }
+                    </div>
+                </div>
+
+
+
+            )}
+
+        </>
+
 
     )
 }

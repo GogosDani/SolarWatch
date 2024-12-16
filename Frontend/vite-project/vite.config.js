7122abd6640592668a087f8c71489e601ee704e9
GogosDani <gogosdaniel04@gmail.com>
2024-12-16 19:50:28 +0100

    frontend docker works fine

--- Frontend/vite-project/vite.config.js
+++ Frontend/vite-project/vite.config.js
@@ -5,11 +5,11 @@ export default defineConfig({
   base: "/",
   plugins: [react()],
   preview: {
-    port: 4000,
+    port: 3000,
     strictPort: true,
   },
   server: {
-    port: 4000,
+    port: 3000,
     strictPort: true,
     host: true,
     origin: "http://0.0.0.0:3000",

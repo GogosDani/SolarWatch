6fa9d8210986a8f92bbb31af6b714cefc2df7d91
GogosDani <gogosdaniel04@gmail.com>
2024-12-19 11:56:35 +0100

    Admin page dummy structure, add scss file

--- Frontend/vite-project/src/Pages/AdminPage.jsx
+++ Frontend/vite-project/src/Pages/AdminPage.jsx
@@ -1,13 +1,46 @@
 import { useState, useEffect } from "react"
 import AdminHeader from "../Components/AdminHeader"
+import './AdminPage.scss'
 
 export default function AdminPage() {
+    const [cities, setCities] = useState({});
+    const [solars, setSolars] = useState({});
+
+    useEffect(() => {
+
+    }, [])
+
+    useEffect(() => {
+
+    }, [])
 
     return (
         <>
             <AdminHeader />
-            <div className="admin-title-div">
-                <p className="admin-page-title"> SOLAR ADMIN PAGE </p>
+            <div className="admin-page-div">
+                <div className="solars">
+                    <div className="solar">
+                        <p> a </p>
+                        <p> b </p>
+                    </div>
+                    <div className="solar">
+                        <p> c </p>
+                        <p> d </p>
+                    </div>
+                </div>
+                <div className="cities">
+                    <div className="city">
+                        <p> Name: Szigethalom</p>
+                        <p> Latitude: 10.10</p>
+                        <p> Longitude: 10.10</p>
+
+                    </div>
+                    <div className="city">
+                        <p> Name: Ráckeve</p>
+                        <p> Latitude: 10.10</p>
+                        <p> Longitude: 41.41</p>
+                    </div>
+                </div>
             </div>
 
         </>

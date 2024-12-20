3bea57a46b760e8a733fa8af5d902c724ee194e7
GogosDani <gogosdaniel04@gmail.com>
2024-12-20 13:59:52 +0100

    debug: change latitude and longitude input type to decimal

--- Frontend/vite-project/src/Components/CityForm.jsx
+++ Frontend/vite-project/src/Components/CityForm.jsx
@@ -15,9 +15,9 @@ export default function CityForm({ apiMethod, city }) {
         <div className="search-form-div">
             <form className="search-form" onSubmit={(e) => apiMethod(cityData, e)}>
                 <label className="search-form-label"> Longitude </label>
-                <input className="search-form-input" value={cityData.longitude} type="number" onChange={(e) => setCityData(prev => ({ ...prev, longitude: e.target.value }))} />
+                <input className="search-form-input" value={cityData.longitude} type="decimal" onChange={(e) => setCityData(prev => ({ ...prev, longitude: e.target.value }))} />
                 <label className="search-form-label"> Latitude </label>
-                <input className="search-form-input" value={cityData.latitude} type="number" onChange={(e) => setCityData(prev => ({ ...prev, latitude: e.target.value }))} />
+                <input className="search-form-input" value={cityData.latitude} type="decimal" onChange={(e) => setCityData(prev => ({ ...prev, latitude: e.target.value }))} />
                 <label className="search-form-label"> Name </label>
                 <input className="search-form-input" value={cityData.name} type="text" onChange={(e) => setCityData(prev => ({ ...prev, name: e.target.value }))} />
                 <button className="search-submit"> Submit </button>
